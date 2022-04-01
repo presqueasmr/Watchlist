dfbc2910e5cd59a4ca745ff9cdafd77de92b18e9
Auriane-Mirana SICARD-RAZAKA <auriane1103@gmail.com>
2022-04-01 17:32:06 +0200

    fix root resource 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root "home#index"
   resources :films
   resources :users
 
   devise_for :users, :path => '', :path_names => { :sign_in => "Login", :sign_out => "Logout", :sign_up => "register" }
-  root "home#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
