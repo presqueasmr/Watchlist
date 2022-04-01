52fa66f4cb7704d0777af0e63e4f6661330f2cb7
Auriane-Mirana SICARD-RAZAKA <auriane1103@gmail.com>
2022-04-01 17:26:20 +0200

    fix root resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :films
-  ressources :users
+  resources :users
 
   devise_for :users, :path => '', :path_names => { :sign_in => "Login", :sign_out => "Logout", :sign_up => "register" }
   root "home#index"
